4bad647f77232b7f7c7fe92201d3d179e5dfc22d
Makoto Fujiwara <makoto@ki.nu>
2015-02-03 12:16:33 +0900

    New file to separate customizing part

---
+++ environment-sample.mk
@@ -0,0 +1,6 @@
+
+CHECK_UPDATE=	/export/git-work/check-update/check-update
+MERGE=		/export/git-work/check-update/merge-check-update
+COLLECT_STATS=	/export/git-work/check-update/collect-stats
+
+DIRECTORY=	${HOME}/public_html/pkgsrc/check-update
