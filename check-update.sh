14b9d55cf8b85fe5f4ee7946f5210bb199d47f8d
Makoto Fujiwara <makoto@ki.nu>
2015-02-08 17:38:25 +0900

    Add progressing message

--- check-update.sh
+++ check-update.sh
@@ -9,7 +9,7 @@ JOBS=36
 
 # Check if required packages installed
 FAIL=0
-echo ' *** Checking packages required'
+echo ' *** (1) Checking packages required'
 for p in httping curl git-base p5-Net-DNS *rubygems mozilla-rootcerts; do
    NOT_EXIST=0
    pkg_info -q -c $p  > /dev/null 2>&1
@@ -22,17 +22,25 @@ for p in httping curl git-base p5-Net-DNS *rubygems mozilla-rootcerts; do
 
 if [ $FAIL = 1 ] ; then exit; fi
 
-echo ' *** Updating from git repository'
+echo ' *** (2) Updating from git repository'
 
 git pull
 
+echo ' *** (3) Updating from cvs repository'
 if [ ! -d /tmp/pkgsrc ] ; then
   (cd /tmp ;        time -c cvs -Q -d ${CVSROOT} co pkgsrc )
 else
   (cd /tmp/pkgsrc;        time -c cvs -Q update -dPA . )
 fi
 
+echo ' *** (4) Applying mk/fetch/sites.mk patch'
+
 (cd /tmp/pkgsrc ; patch -s -p0 < ${SITE_PATCH} )
 
+echo ' *** (5) Applying mk/fetch/sites.mk patch'
+
 if [ ! -f environment.mk ]; then cp environment-sample.mk environment.mk ; fi
+
+echo ' *** (6) Now real make '
+
 env PKGSRC=/tmp/pkgsrc make -j ${JOBS}
