4c5eefe43fbdff8a487ddfb77f56590f1413b5d8
Makoto Fujiwara (CF-S9) <makoto@ki.nu>
2017-03-30 22:49:54 +0900

    (check-update-sample.sh) make it as general purpose

--- check-update-sample.sh
+++ check-update-sample.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 CVSROOT=:pserver:anoncvs@anoncvs.jp.netbsd.org:/cvs/cvsroot
+CVSROOT=:pserver:anoncvs@anoncvs.netbsd.org:/cvsroot
+
+# dedicated pkgsrc directory for this purpose
+TMP=/tmp
+TMP_PKGSRC=${TMP}/pkgsrc
+
 WIPROOT=:pserver:anoncvs@pkgsrc-wip.cvs.sourceforge.net:/cvs/pkgsrc-wip/cvsroot
 SITE_PATCH=${HOME}/public_html/pkgsrc/mk/fetch-sites.mk.diff
 # ---------------------------
@@ -22,27 +28,31 @@ for p in httping curl git-base p5-Net-DNS p5-Algorithm-Diff mozilla-rootcerts w3
 
 if [ $FAIL = 1 ] ; then exit; fi
 
+if [ -d .git ] ; then
 echo ' *** (2) Updating from git repository'
 
 git pull
 HASH=`git log --format="%H" -1`
 export HASH
-
-echo ' *** (3) Updating from cvs repository'
-if [ ! -d /tmp/pkgsrc ] ; then
-  (cd /tmp ;        time -c cvs -Q -d ${CVSROOT} co pkgsrc )
-else
-  (cd /tmp/pkgsrc;        time -c cvs -Q update -dPA . )
 fi
 
+echo ' *** (3) Updating from cvs repository'
+if [ ! -d ${TMP_PKGSRC} ] ; then
+  (cd ${TMP};           time -c cvs -Q -d ${CVSROOT} co pkgsrc )
+else
+  (cd ${TMP_PKGSRC};    time -c cvs -Q update -dPA . )
+fi
+
+if [ -f ${SITE_PATCH} 
 echo ' *** (4) Applying mk/fetch/sites.mk patch'
+fi
 
-(cd /tmp/pkgsrc ; patch -s -N -p0 < ${SITE_PATCH} )
+(cd ${TMP_PKGSRC} ; patch -s -N -p0 < ${SITE_PATCH} )
 
-echo ' *** (5) Applying mk/fetch/sites.mk patch'
+echo ' *** (5) copy environment-sample.mk if environment.mk not found '
 
 if [ ! -f environment.mk ]; then cp environment-sample.mk environment.mk ; fi
 
 echo ' *** (6) Now real make '
 
-env PKGSRC=/tmp/pkgsrc make -j ${JOBS}
+env PKGSRC=${TMP_PKGSRC} make -j ${JOBS}
