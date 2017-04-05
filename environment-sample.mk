13e5ae39a950a8b7071d7c55021a93bac2e2670a
Makoto Fujiwara <makoto@ki.nu>
2017-04-05 20:33:15 +0900

    (environment-sample.mk) SUMMARY_DIFF=... was  missing

--- environment-sample.mk
+++ environment-sample.mk
@@ -2,5 +2,6 @@
 CHECK_UPDATE=	/usr/pkg/sbin/check-update
 MERGE=		/usr/pkg/share/check-update/merge-check-update
 COLLECT_STATS=	/usr/pkg/share/check-update/collect-stats
+SUMMARY_DIFF=	/usr/pkg/share/check-update/summary-diff
 
 DIRECTORY=	/var/check-update/htdocs
