3d588130ba5c87ce7a0be6c0bf4cc34ac0f60cc9
Makoto Fujiwara (CF-SX3) <makoto@ki.nu>
2019-10-20 06:34:33 +0900

    (check-update-sample.sh) Prerequisite py-pip

--- check-update-sample.sh
+++ check-update-sample.sh
@@ -16,7 +16,7 @@ JOBS=36
 # Check if required packages installed
 FAIL=0
 echo ' *** (1) Checking packages required'
-for p in httping curl git-base p5-Net-DNS p5-Algorithm-Diff mozilla-rootcerts w3m; do
+for p in httping curl git-base p5-Net-DNS p5-Algorithm-Diff mozilla-rootcerts w3m py-pip; do
    NOT_EXIST=0
    pkg_info -q -c $p  > /dev/null 2>&1
    RC=$?
