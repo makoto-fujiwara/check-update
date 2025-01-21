82dc2f59118db5599761ec67a3121845b742fa4b
Makoto Fujiwara <makoto@if.t.u-tokyo.ac.jp>
2025-01-22 08:20:38 +0900

    ham_trustedQSL special

--- pm/ham_trustedQSL.pm
+++ pm/ham_trustedQSL.pm
@@ -2,12 +2,13 @@ package external;
 sub ham_trustedQSL($) {
 #     w3m -dump -T text http://www.arrl.org/tqsl-download >  tqsl-download.txt
 #     egrep 'TQSL [0-9.]*' tqsl-download.txt
+    my @myCANDIDATE;
 
     my $pid = open(W3M, "w3m -dump -T text http://www.arrl.org/tqsl-download |");
     while (<W3M>)  {
-        if (/TQSL ([0-9.]*)/ ) { my $string = $1; push(@CANDIDATE, $string);}
+        if (/TQSL ([0-9.]*)/ ) { my $string = $1; push(@myCANDIDATE, $string);}
     }
     close(W3M);
-    return @CANDIDATE;
+    return @myCANDIDATE;
 }
 1;
